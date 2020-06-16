fffac65f17f6fc7277b1a186541da4abae8d5b3f
timmy31113 <timmy.xqz21@gmail.com>
2020-06-16 10:43:56 +0200

    Initialized Maven Java project
    This will make dependencies a lot easier later on.

---
+++ src/main/java/de/sanz_berlin/tim/db/App.java
@@ -0,0 +1,13 @@
+package de.sanz_berlin.tim.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
