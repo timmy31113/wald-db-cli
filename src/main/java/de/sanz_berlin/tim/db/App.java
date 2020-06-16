47f1428ce909c796abed93295d019f39bf931ebe
timmy31113 <timmy.xqz21@gmail.com>
2020-06-16 11:52:41 +0200

    Implemented database connection
    It's not much but it works :)

--- src/main/java/de/sanz_berlin/tim/db/App.java
+++ src/main/java/de/sanz_berlin/tim/db/App.java
@@ -1,13 +1,42 @@
 package de.sanz_berlin.tim.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		Connection dbcon;
+		Statement stmt;
+
+		try {
+			dbcon = DriverManager.getConnection("jdbc:postgresql://localhost/wald");
+			stmt = dbcon.createStatement();
+		} catch (SQLException e) {
+			System.err.println("Can't connect to database: " + e);
+			System.exit(1);
+			return;
+		}
+
+		try {
+			ResultSet rs = stmt.executeQuery("select * from schädigungen");
+			while (rs.next()) {
+				System.out.println("-------------------------------");
+				System.out.println("Nummer: " + rs.getInt("nummer"));
+				System.out.println("Art: " + rs.getString("art"));
+				System.out.println("Datum: " + rs.getDate("datum"));
+				System.out.println("Baum: " + rs.getInt("baum"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
